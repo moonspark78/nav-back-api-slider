9d489a15886831eb6fb76bfa431543958a27ba82
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-11 17:05:00 +0100

    :art: nav  :art:

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,20 +6,23 @@ export const Navbar = () => {
 
     const navRef = useRef();
     
+    const showNav = () => {
+      navRef.current.classList.toggle("responsive-nav");
+    };
 
   return (
     <header>
             <h3>Logo</h3>
-            <nav>
+            <nav ref={navRef}>
                 <a href='/#'>Home</a>
                 <a href='/#'>Api</a>
                 <a href='/#'>Slider</a>
                 <a href='/#'>About me</a>
-                <button className="nav-btn nav-close">
+                <button className="nav-btn nav-close" onClick={showNav}>
                     <FaTimes/>
                 </button>
             </nav>
-            <button className="nav-btn">
+            <button className="nav-btn" onClick={showNav}>
                 <FaBars/>
             </button>
     </header>
