af078836d53c43105f6288024e6e1cc74b201108
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-13 16:12:14 +0100

    :art: log the data on the console  :art:

--- src/Components/Api.jsx
+++ src/Components/Api.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./astyle.css"
 
 /* https://rickandmortyapi.com/api/character */
@@ -6,10 +6,20 @@ import "./astyle.css"
 export const Api = () => {
     const [character, setCharacter] = useState([]);
 
-    
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const response = await fetch("https://rickandmortyapi.com/api/character");
+        const data = await response.json();
+        console.log(data.results);
+      };
+      fetchData();
+    },[]);
+   
+
   return (
-    <div className='to'>
-      {/* <p>Bonjour</p> */}
+    <div>
+
     </div>
   )
 }
